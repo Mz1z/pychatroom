2cca173ded1aabb1e4f20ea34d784778663c21f7
Mz1 <mzi_mzi@163.com>
2022-10-10 14:21:38 +0800

    improve room datastruct and functions

--- pychatroom.py
+++ pychatroom.py
@@ -29,7 +29,11 @@ index_html = '''
 <div id='header'>
 	<h4>说明：你可以在网址后面加任意字符进入任意房间，例如加上"cybersecurity"变成"http://xxxx.com:5000/cybersecurity"</h4>
 	<h4>你也可以什么都不做，就在大厅聊天</h4>
-	<h3>当前聊天室: {{ _path }}</h3>
+	{% if _path|length != 0 %}
+		<h3>当前聊天室: {{ _path }}</h3>
+	{% else %}
+		<h3>当前位置：大厅</h3>
+	{% endif %}
 </div>
 
 <hr/>
@@ -42,7 +46,7 @@ index_html = '''
 
 <hr/>
 	
-<div id="msg"></div>
+<div id="msg" style=" overflow-y:auto; overflow-x:auto; width:400px; height:400px;"></div>
 
 </div>
 	
@@ -100,6 +104,9 @@ index_html = '''
     function onMessage(event){
         console.log("onMessage"+event.data);
         document.getElementById("msg").innerHTML += "<p>"+event.data+"</p>"
+        
+        //消息框滚动最下
+		$("#msg").scrollTop($("#msg").prop('scrollHeight'));
     };
 
     function onClose(event){
@@ -128,8 +135,17 @@ index_html = '''
     function close(event){
         webSocket.close();
     }
-</script>
+    
+// 回车按键处理
+$(document).keyup(function (e) {//捕获文档对象的按键弹起事件
+    if (e.keyCode == 13) {//按键信息对象以参数的形式传递进来了
+        //此处编写用户敲回车后的代码
+        start();
+    }
+});
 
+
+</script>
 
 </html>
 '''
@@ -143,7 +159,15 @@ import time
 # Websocket服务端
 class WebsocketChatServer():
 	def __init__(self):
-		self.conn = {}  # 连接字典 房间名:[conn1, conn2]
+		'''连接字典 
+		self.conn= {
+			房间名:{ 
+				unames: [uname1, uname2],
+				conns: [conn1, conn2]
+			}
+		}
+		'''
+		self.conn = {}  
 
 	async def run(self, port):
 		start_server = websockets.serve(self.handler, "", port)
@@ -154,36 +178,66 @@ class WebsocketChatServer():
 	# 处理消息用的handle
 	async def handler(self, websocket, path):
 		uname = await websocket.recv()   # 等待获取用户名
+		# 用户名检查
+		if uname == '':
+			await websocket.send('用户名不能为空，请刷新后重试')
+			return
 		
 		# 进入房间
 		print(f'\n用户:{uname} 进入房间：{path}')   # 当前房间路径
-		if path in self.conn:
-			self.conn[path].append(websocket)   # 添加当前连接
+		if path in self.conn:   # 存在当前房间
+			# 判断用户名是否重复
+			if uname in self.conn[path]['unames']:
+				# 用户名重复
+				await websocket.send('用户名与房间中用户重复，请刷新后重试')
+				return
+			self.conn[path]['conns'].append(websocket)   # 添加当前连接
+			self.conn[path]['unames'].append(uname)     # 添加当前用户名
+			await websocket.send(
+				f'<b style="color:blue;">当前房间在线用户{len(self.conn[path]["unames"])}人: {str(self.conn[path]["unames"])}</b>'
+			)   # 发送当前人数和用户
 		else:
-			self.conn[path] = [websocket]    # 添加当前连接
+			self.conn[path] = {}
+			self.conn[path]['conns'] = [websocket]    # 添加当前连接
+			self.conn[path]['unames'] = [uname]
+			await websocket.send(
+				f'<b style="color:blue;">当前房间在线用户{len(self.conn[path]["unames"])}人: {str(self.conn[path]["unames"])}</b>'
+			)   # 发送当前人数和用户
 		print(f'当前房间: {self.conn}')
+		# 进入房间广播
+		await self.boardcast(self.conn[path]['conns'], f'<b style="color:green;">用户{uname}进入房间！</b>')
 		
 		# 循环处理消息
 		while True:
 			try:
 				msg = await websocket.recv()
 				# 分发消息给房间里的用户
-				for conn in self.conn[path]:
-					await conn.send(f'{uname}: {msg}')
+				await self.boardcast(self.conn[path]['conns'], f'<b>{uname}:</b> {msg}')
 			except websockets.ConnectionClosedOK:
 				break
 			except websockets.ConnectionClosedError:
 				break
 			print(f"recv: {msg}")
 			
-		self.conn[path].remove(websocket)  # 移除连接
+		self.conn[path]['conns'].remove(websocket)  # 移除连接
+		self.conn[path]['unames'].remove(uname)  # 移除用户名
+		
+		# 退出房间广播
+		await self.boardcast(self.conn[path]['conns'], f'<b style="color:red;">用户{uname}离开了房间！</b>')
+		
+		
 		# 判断房间有没有人
 		_conn = dict(self.conn)  # 复制一个副本
 		for room in _conn:
-			if len(_conn[room]) == 0:   # 房间没人了，销毁房间
+			if len(_conn[room]['conns']) == 0:   # 房间没人了，销毁房间
 				self.conn.pop(room)
-		print(self.conn)
+		print(f'当前房间: {self.conn}')
 		print('  > close a connection')
+		
+	# 向分组发送广播
+	async def boardcast(self, conns, msg):
+		for conn in conns:
+			await conn.send(msg)
 
 
 # 初始化和定义flask
